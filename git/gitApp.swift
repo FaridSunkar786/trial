232f521cfbced1e6a2ef82278d7518b347512f1b
Faridsunkar786 <Divisionrepublic45@gmail.com>
2025-05-27 10:53:19 +0800

    Initial Commit

---
+++ git/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  git
+//
+//  Created by Farid Ali on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gitTests/gitTests.swift
@@ -0,0 +1,17 @@
+//
+//  gitTests.swift
+//  gitTests
+//
+//  Created by Farid Ali on 27/05/25.
+//
+
+import Testing
+@testable import git
+
+struct gitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
